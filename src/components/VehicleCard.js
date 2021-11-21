40ca71f9b706d3bd31147710c7b20e214e51132c
Maduri Vassaramo <maduri@vinterior.co>
2021-11-21 13:56:02 +0100

    make whole vehicleCard trigger modal display

--- src/components/VehicleCard.js
+++ src/components/VehicleCard.js
@@ -5,14 +5,13 @@ const VehicleCard = props => {
   const { photo, make, model, price, range } = props.vehicle
 
   return (
-    <div className="vehicle-card">
+    <div className="vehicle-card" onClick={() => props.displayModal(props.vehicle)}>
       <img className="vehicle-card-img" src={photo} alt="vehicle" />
       <div className="vehicle-card-content">
         <p>Make: {make}</p>
         <p>Model: {model}</p>
         <p>Price: {price}</p>
-        <p>{range.distance}</p>
-        <button className="" onClick={() => props.displayModal(props.vehicle)}>See More</button>
+        <p>Range: {range.distance} {range.unit}</p>
       </div>
     </div>
   )
